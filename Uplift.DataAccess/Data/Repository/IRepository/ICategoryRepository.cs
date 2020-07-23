5e191d74f89157f1bcff238c97a4923882df2ffd
abayomi igwubor <igwubor@outlook.com>
2020-07-23 14:32:27 +0100

    implementedIRepositoryInterfaceOnCategory

---
+++ Uplift.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Uplift.Models;
+
+namespace Uplift.DataAccess.Data.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+    }
+}
