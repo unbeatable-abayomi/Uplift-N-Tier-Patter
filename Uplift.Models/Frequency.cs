da363e95c7272a32a1dcec6c9eacec6b3e4a0562
abayomi igwubor <igwubor@outlook.com>
2020-07-28 19:14:26 +0100

    completedCRUDOnFrequency

---
+++ Uplift.Models/Frequency.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Uplift.Models
+{
+   public class Frequency
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Frequency Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Frequency Count")]
+        public int FrequencyCount { get; set; }
+    }
+}
